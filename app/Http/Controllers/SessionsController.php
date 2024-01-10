fe933c8cfd1e2e26ef521d24530647b53cb3f504
tyler <mr.anderson@realtourvision.com>
2024-01-09 21:38:08 -0500

    login sessions

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -3,9 +3,34 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\ValidationException;
 
 class SessionsController extends Controller
 {
+
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    public function store()
+    {
+        $attributes = request()->validate([
+            'email' => ['required', Rule::exists('users', 'email')],
+            'password' => 'required'
+        ]);
+
+        if (!auth()->attempt($attributes)) {
+            throw ValidationException::withMessages([
+                'email' => 'Your Provided Credentials could not be verified.'
+            ]);
+        }
+
+        session()->regenerate();
+        return redirect('/')->with('success', 'Welcome Back!');
+    }
+
     public function destroy()
     {
         auth()->logout();
