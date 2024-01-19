9b1de5640c51db6b1a3031eeb9ac26e2157f6477
tyler <mr.anderson@realtourvision.com>
2024-01-18 21:15:24 -0500

    started file system

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,8 +1,10 @@
 <x-layout>
-
-    <section class="px-6 py-8">
-        <x-panel class="max-w-lg mx-auto">
-            <form action="/admin/posts" method="post">
+    <section class="py-8 max-w-lg mx-auto">
+        <h1 class="text-lg font-bold mb-4">
+            Publish New Post
+        </h1>
+        <x-panel class="">
+            <form action="/admin/posts" method="post" enctype="multipart/form-data">
                 @csrf
 
                 <div class="mb-6">
@@ -37,6 +39,24 @@
                     >
 
                     @error('slug')
+                        <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+                    @enderror
+                </div>
+
+                <div class="mb-6">
+                    <label for="thumbnail" class="block mb-2 uppercase font-bold text-xs text-gray-700">
+                        Thumbnail
+                    </label>
+
+                    <input class="border border-gray-400 p-2 w-full"
+                    type="file"
+                    name="thumbnail"
+                    id="thumbnail"
+                    value="{{ old('thumbnail')}}"
+                    required
+                    >
+
+                    @error('thumbnail')
                         <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
                     @enderror
                 </div>
