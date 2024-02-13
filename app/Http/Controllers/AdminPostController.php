f806253fa4b607d2df56c0d38277321550b0272f
tyler <mr.anderson@realtourvision.com>
2024-02-12 22:58:07 -0500

    conintued journey

--- app/Http/Controllers/AdminPostController.php
+++ app/Http/Controllers/AdminPostController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class AdminPostController extends Controller
 {
@@ -11,6 +13,42 @@ class AdminPostController extends Controller
     {
         return view('admin.posts.index', [
             'posts' => Post::paginate(50)
+        ]);
+    }
+
+    public function create()
+    {
+        return view('admin.posts.create', [
+            'categories' => Category::all(),
+            'post' => new Post(),
+
+        ]);
+    }
+
+    public function store()
+    {
+        $attributes = request()->validate([
+            'title' => 'required',
+            'thumbnail' => ['required', 'image'],
+            'slug' => ['required', Rule::unique('posts', 'slug')],
+            'excerpt' => 'required',
+            'body' => 'required',
+            'category_id' => ['required', Rule::exists('categories', 'id')]
+        ]);
+
+        $attributes['user_id'] = auth()->id();
+        $attributes['thumbnail'] = request()->file('thumbnail')->store('thumbnails');
+
+        Post::create($attributes);
+
+        return redirect('/');
+    }
+
+    public function edit(Post $post)
+    {
+        return view('admin.posts.edit', [
+            'categories' => Category::all(),
+            'post' => $post
         ]);
     }
 }
--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -1,13 +1,13 @@
 <x-layout>
-    <x-setting heading="Publish New Post">
+    <x-setting :heading="'Edit Post ' . $post->title">
         <form action="/admin/posts" method="post" enctype="multipart/form-data">
             @csrf
 
-            <x-form.input name="title" />
-            <x-form.input name="slug" />
-            <x-form.input name="thumbnail" type="file" />
-            <x-form.textarea name="excerpt" />
-            <x-form.textarea name="body" />
+            <x-form.input name="title" :value="old('title', $post->title)" />
+            <x-form.input name="slug" :value="old('slug', $post->slug)" />
+            <x-form.input name="thumbnail" type="file" :value="old('thumbnail', $post->thumbnail)" />
+            <x-form.textarea name="excerpt">{{ old('excerpt', $post->excerpt) }}</x-form.textarea>
+            <x-form.textarea name="body">{{ old('body', $post->body) }}</x-form.textarea>
 
             <x-form.field>
                 <x-form.label name="category" />
@@ -21,7 +21,7 @@
                 @foreach ($categories as $category)
                 <option
                     value="{{ $category->id }}"
-                    {{ old('category_id') == $category->id ? 'selected' : ''}}>
+                    {{ old('category_id', $post->category_id) == $category->id ? 'selected' : ''}}>
                     {{ ucwords($category->name) }}
                 </option>
                 @endforeach
